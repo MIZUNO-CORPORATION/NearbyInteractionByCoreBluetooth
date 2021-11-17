6a23b71d38f59d0465cb8f365c334417fd06393f
YutoKase <kase@mizuno.co.jp>
2021-11-17 13:30:35 +0900

    Initial Commit

---
+++ NearbyInteractionByCoreBluetooth WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,26 @@
+//
+//  InterfaceController.swift
+//  NearbyInteractionByCoreBluetooth WatchKit Extension
+//
+//  Created by AM2190 on 2021/11/17.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+}
---
+++ NearbyInteractionByCoreBluetooth/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NearbyInteractionByCoreBluetooth
+//
+//  Created by AM2190 on 2021/11/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
